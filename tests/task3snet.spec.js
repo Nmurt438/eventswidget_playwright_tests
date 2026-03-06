1600e607f16226295599c6d9c36173abe1f6d242
Nmurt438 <hello.murtazina@gmail.com>
2026-03-06 16:01:48 +0300

    commit only changed files

--- tests/task3snet.spec.js
+++ tests/task3snet.spec.js
@@ -1,18 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('generate widget preview', async ({ page }) => {
-  await page.goto('https://dev.3snet.info/eventswidget/', {
-  waitUntil: 'domcontentloaded',
-  timeout: 60000
-});
-  await page.locator(".checkselect-over").first().click()
+test('test', async ({ page }) => {
+  await page.goto('https://dev.3snet.info/eventswidget/');
+  await page.locator('.checkselect-over').first().click();
   await page.getByText('Affiliate').click();
-
-  await page.locator('.input-item .checkselect-over').nth(1).waitFor();
-  await page.waitForSelector('.checkselect-dropdown');
-  await page.locator('.checkselect-dropdown').getByText('Выбрать все').click();
-  await page.waitForTimeout(500);
+  await page.locator('div').filter({ hasText: 'Шаг 2' }).nth(4).click();
+  await page.locator('div:nth-child(2) > .input-item > .checkselect > .checkselect-control > .checkselect-over').click();
+  await page.getByText('Бразилия').click();
+  await page.getByText('Шаг 2 Выберите страны Выбрано: 1').click();
+  await page.locator('.radio__square').first().click();
   await page.getByRole('button', { name: 'Сгенерировать превью' }).click();
-
-  await expect(page.locator('iframe')).toBeVisible();
+  await page.getByRole('button', { name: 'Скопировать код' }).click();
 });
